f6f63fe6e30247cc67301393b38d0e96c79f99d8
sai-sridhar <ssp2503@gmail.com>
2015-07-07 18:40:17 -0400

    Fixed Economy Rate and Maiden over calculation.

--- CricketifyApp/CricketifyApp/Scripts/CricketifyApp/Scorecard.js
+++ CricketifyApp/CricketifyApp/Scripts/CricketifyApp/Scorecard.js
@@ -9,6 +9,7 @@ app.controller('ScorecardController', function ($scope) {
     $scope.batsmanTwo = "Paaone";
     $scope.runs = 0;
     $scope.totalBls = 0;
+    $scope.totalBlsBowled = 0;
     $scope.fours = 0;
     $scope.sixers = 0;
     $scope.bowlerOne = "S Bond";
@@ -22,7 +23,7 @@ app.controller('ScorecardController', function ($scope) {
             return 0;
         }
         else {
-            return ($scope.bowlingRuns / $scope.overs).toFixed(2);
+            return (($scope.bowlingRuns / $scope.totalBlsBowled) * 6).toFixed(2);
         }
     }
     $scope.strikeRate = function () {
@@ -35,14 +36,22 @@ app.controller('ScorecardController', function ($scope) {
     }
     $scope.handleScorecardClick = function (id) {        
         if (id == 0 || id == 1 || id == 2 || id == 3 || id == 4 || id == 5 || id == 6) {
+            $scope.totalBlsBowled++;
             oversDecimalCount++;
+            if (id == 0) {
+                consecutiveDotBallsCount++;
+            }
             if (oversDecimalCount == 6) {
                 oversCount++;
                 $scope.overs = oversCount;
                 oversDecimalCount = 0.0;
+                if (consecutiveDotBallsCount == 6) {
+                        $scope.maidens++;
+                }
+                consecutiveDotBallsCount = 0;
             }
             else {
-                $scope.overs = oversCount + "." + oversDecimalCount;
+                $scope.overs = oversCount + "." + oversDecimalCount;                
             }
             $scope.bowlingRuns += id;
             $scope.totalBls++;
@@ -55,19 +64,13 @@ app.controller('ScorecardController', function ($scope) {
                 consecutiveDotBallsCount = 0;
                 $scope.sixers++;
             }
-            else if (id == 0) {
-                consecutiveDotBallsCount++;
-                if (consecutiveDotBallsCount == 6) {
-                    $scope.maidens++;
-                    consecutiveDotBallsCount = 0;
-                }
-            }
             else if (id == 1 || id == 2 || id == 3 || id == 5) {
                 consecutiveDotBallsCount = 0;
             }
         }
         else if (id == "Wicket") {
             $scope.wickets++;
+            $scope.totalBlsBowled++;
         }
     }
 });
