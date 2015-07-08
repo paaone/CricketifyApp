bd1f6290daffdf74b5b060ecd2facc8b4e1e45a3
paaone <paaone@gmail.com>
2015-07-08 02:41:40 -0400

    Updating Batsman Stats
    - Updating individual batsman score
    - simulating fall of wicket (need to handle next batsman)

--- CricketifyApp/CricketifyApp/Scripts/CricketifyApp/Scorecard.js
+++ CricketifyApp/CricketifyApp/Scripts/CricketifyApp/Scorecard.js
@@ -27,6 +27,36 @@ function endOfOver() {
     DotBallsCount = 0;
 
     // switch bowlers
+    // rotate strike
+}
+
+function updateBatsmanStats(batsman, action) {
+    if (isNaN(action)) { // It is either a wicket or an extra
+        if (action = "W") {
+            batsman.status = "OUT";
+            batsman.ballsPlayed++;
+        }
+    }
+    else {
+        if (action == 4) {
+            batsman.fours++;
+        }
+        if (action == 6) {
+            batsman.sixes++;
+        }
+
+        batsman.Runs = batsman.Runs + action;
+        batsman.ballsPlayed++;
+    }
+
+    batsman.SR = strikeRate(batsman.Runs, batsman.ballsPlayed);
+}
+
+function rotateStrike(batsmanOnStrike, batsmanNonStrike, action){
+    if (action == 1 || action == 3 || action == 5) {
+        batsmanOnStrike.status = "non-strike";
+        batsmanNonStrike.status = "on-strike";
+    }
 }
 
 function updateStats(action, balls) {
@@ -66,26 +96,37 @@ var app = angular.module('myApp', []);
 app.controller('ScorecardController', function ($scope) {
     $scope.actionPerBall = [{ zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, wicket: "W", extra: "Extras" }];
 
-    $scope.batsmen =
-        [{
-            Name: "Sai",
+    $scope.battingTeam = [{
+        TeamName: "Spades",
+        Players: [{
+            Name: "Sai Prashant",
             Runs: 0,
             ballsPlayed: 0,
             fours: 0,
             sixes: 0,
             SR: 0.0,
-            onStrike: "true"
+            status: "on-strike"
         },
         {
-            Name: "Pavan",
+            Name: "Pavan Samprati",
             Runs: 0,
             ballsPlayed: 0,
             fours: 0,
             sixes: 0,
-            SR: 0,
-            onStrike: "false"
-        }];
-
+            SR: 0.0,
+            status: "non-strike"
+        },
+            {
+                Name: "Sandeep N",
+                Runs: 0,
+                ballsPlayed: 0,
+                fours: 0,
+                sixes: 0,
+                SR: 0.0,
+                status: "DNB"
+            }
+        ]
+    }];
 
     $scope.batsmanOne = "Sai";
     $scope.batsmanTwo = "Paaone";
@@ -116,6 +157,18 @@ app.controller('ScorecardController', function ($scope) {
         $scope.wickets = wickets;
         $scope.sixers = sixes;
         $scope.fours = fours;
+
+        var batsmanOnStrike = $scope.battingTeam[0].Players.filter(function (obj) {
+            return obj.status == "on-strike";
+        })
+
+        var batsmanNonStrike = $scope.battingTeam[0].Players.filter(function (obj) {
+            return obj.status == "non-strike";
+        })
+
+        updateBatsmanStats(batsmanOnStrike[0], id);
+        rotateStrike(batsmanOnStrike[0], batsmanNonStrike[0], id);
+
 
     }
 
