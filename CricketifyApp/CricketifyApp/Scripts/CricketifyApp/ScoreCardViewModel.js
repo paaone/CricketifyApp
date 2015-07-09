33c6f1dc76b1546c6d7c7d7d14ca16cf056715c0
paaone <paaone@gmail.com>
2015-07-09 19:20:55 -0400

    Refactoring
    - restructured the code a bit

---
+++ CricketifyApp/CricketifyApp/Scripts/CricketifyApp/ScoreCardViewModel.js
@@ -0,0 +1,37 @@
+﻿function getBattingTeam(teamId) {
+
+    // eventually this will be a call to the server which will return the batting team. 
+    battingTeam = [{
+        TeamName: "Spades",
+        Players: [{
+            Name: "Sai Prashant",
+            Runs: 0,
+            ballsPlayed: 0,
+            fours: 0,
+            sixes: 0,
+            SR: 0.0,
+            status: "on-strike"
+        },
+        {
+            Name: "Pavan Samprati",
+            Runs: 0,
+            ballsPlayed: 0,
+            fours: 0,
+            sixes: 0,
+            SR: 0.0,
+            status: "non-strike"
+        },
+            {
+                Name: "Sandeep N",
+                Runs: 0,
+                ballsPlayed: 0,
+                fours: 0,
+                sixes: 0,
+                SR: 0.0,
+                status: "DNB"
+            }
+        ]
+    }];
+
+    return battingTeam;
+}
---
+++ CricketifyApp/CricketifyApp/Scripts/CricketifyApp/ScorecardCalculations.js
@@ -0,0 +1,87 @@
+﻿function displayOvers(balls) {
+    if (balls > 0) {
+        overs = Math.floor(totalBalls / 6);
+        balls = (totalBalls % 6);
+    }
+    if (balls == 0) {
+        endOfOver();
+    }
+    return overs + "." + balls;
+}
+
+function endOfOver() {
+    if (DotBallsCount == 6) {
+        maidens++;
+    }
+    DotBallsCount = 0;
+
+    // switch bowlers
+    // rotate strike
+}
+
+function updateBatsmanStats(batsman, action) {
+    if (isNaN(action)) { // It is either a wicket or an extra
+        if (action = "W") {
+            batsman.status = "OUT";
+            batsman.ballsPlayed++;
+        }
+    }
+    else {
+        if (action == 4) {
+            batsman.fours++;
+        }
+        if (action == 6) {
+            batsman.sixes++;
+        }
+
+        batsman.Runs = batsman.Runs + action;
+        batsman.ballsPlayed++;
+    }
+
+    batsman.SR = strikeRate(batsman.Runs, batsman.ballsPlayed);
+}
+
+function fallOfWicket(batsman, nextBatsman) {
+    batsman.status = "OUT";
+    batsman.ballsPlayed++;
+    nextBatsman.status = "on-strike";
+}
+
+
+function rotateStrike(batsmanOnStrike, batsmanNonStrike, action) {
+    if (action == 1 || action == 3 || action == 5) {
+        batsmanOnStrike.status = "non-strike";
+        batsmanNonStrike.status = "on-strike";
+    }
+}
+
+function updateStats(action, balls) {
+
+    if (isNaN(action)) { // It is either a wicket or an extra
+        if (action = "W") {
+            wickets++;
+        }
+    }
+    else {
+        if (action == 0) {
+            DotBallsCount++;
+        }
+        if (action == 4) {
+            fours++;
+        }
+        if (action == 6) {
+            sixes++;
+        }
+
+        totalRuns = totalRuns + action;
+    }
+}
+
+
+function strikeRate(runs, balls) {
+    return balls == 0 ? 0 : (runs / balls * 100).toFixed(2);
+}
+
+function economyRate(runs, balls) {
+    return balls = 0 ? 0 : (runs / balls * 6).toFixed(2);
+}
